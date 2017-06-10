d005c72049ecc0b4f4d231605656bc203b42b84e
Antreas Pogiatzis <apogiatzis@airpoint.net>
2017-06-10 12:02:55 +0100

    update api

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 1337;
-
+  
+var bodyParser = require ('body-parser')
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
-
+app.use(bodyParser.json());
 
 var routes = require('./api/routes/routes');
 routes(app);
