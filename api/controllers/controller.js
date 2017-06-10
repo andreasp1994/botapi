6479f86552b9a8526906c193727ba8defe754583
Antreas Pogiatzis <apogiatzis@airpoint.net>
2017-06-10 11:49:56 +0100

    updated server

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -3,30 +3,47 @@
 const SPSP = require('ilp').SPSP
 const FiveBellsLedgerPlugin = require('ilp-plugin-bells')
 
-const plugin = new FiveBellsLedgerPlugin({
-  account: 'https://usdledger.online/ledger/accounts/alice',
-  password: 'alice'
-})
-
 exports.make_transfer = function(req, res) {
-  ;(async function () {
-  console.log(req);
-  await plugin.connect()
-  console.log('plugin connected')
+  	;(async function () {
 
-  const payment = await SPSP.quote(plugin, {
-    receiver: 'bob@usdledger.online',
-    sourceAmount: '1',
-  })
+  		const plugin = new FiveBellsLedgerPlugin({
+  			account: 'https://usdledger.online/ledger/accounts/alice',
+  			password: 'alice'
+		})
 
-  console.log('got SPSP payment details:', payment)
+		if(req.method == POST && req.url == '/login') {
 
-  // we can attach an arbitrary JSON object to the payment if we want it
-  // to be sent to the receiver.
-  payment.memo = { message: 'hello!' }
 
-  await SPSP.sendPayment(plugin, payment)
-  console.log('receiver claimed funds!')
-})()
-  res.json('{OK}');
+		var body = "";
+
+		req.on('data', function (chunk) {
+			body += chunk;
+		});
+
+
+		req.on('end', function () {
+			console.log(body);
+		});
+
+		}
+
+		await plugin.connect()
+		console.log('plugin connected')
+
+		const payment = await SPSP.quote(plugin, {
+		receiver: 'bob@usdledger.online',
+		sourceAmount: '1',
+		})
+
+		console.log('got SPSP payment details:', payment)
+
+		// we can attach an arbitrary JSON object to the payment if we want it
+		// to be sent to the receiver.
+		payment.memo = { message: 'hello!' }
+
+		await SPSP.sendPayment(plugin, payment)
+		res.json(payment);
+		console.log('receiver claimed funds!')
+	})()
+  	
 };
