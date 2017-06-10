6b9e94e3cfe2f5999e37e53e722d421c479e8c40
Antreas Pogiatzis <apogiatzis@airpoint.net>
2017-06-10 11:34:34 +0100

    spss

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -1,6 +1,27 @@
 'use strict';
 
+const SPSP = require('ilp').SPSP
+const FiveBellsLedgerPlugin = require('ilp-plugin-bells')
+
 exports.make_transfer = function(req, res) {
-  console.log("Got it");
+  ;(async function () {
+  console.log(req);
+  await plugin.connect()
+  console.log('plugin connected')
+
+  const payment = await SPSP.quote(plugin, {
+    receiver: 'bob@usdledger.online',
+    sourceAmount: '1',
+  })
+
+  console.log('got SPSP payment details:', payment)
+
+  // we can attach an arbitrary JSON object to the payment if we want it
+  // to be sent to the receiver.
+  payment.memo = { message: 'hello!' }
+
+  await SPSP.sendPayment(plugin, payment)
+  console.log('receiver claimed funds!')
+})()
   res.json('{OK}');
 };
--- index.js
+++ index.js
@@ -1,62 +1,28 @@
 'use strict'
 
-const uuid = require('uuid')
-const ILP = require('ilp')
+const SPSP = require('ilp').SPSP
 const FiveBellsLedgerPlugin = require('ilp-plugin-bells')
 
-const sender = new FiveBellsLedgerPlugin({
-  account: 'https://usdledger.online/ledger/accounts/admin',
-  password: 'admin'
-})
-
-const receiver = new FiveBellsLedgerPlugin({
-  account: 'https://usdledger.online/ledger/accounts/bob',
-  password: 'bob1234'
+const plugin = new FiveBellsLedgerPlugin({
+  account: 'https://usdledger.online/ledger/accounts/alice',
+  password: 'alice'
 })
 
 ;(async function () {
-  await receiver.connect()
-  console.log('receiver connected')
+  await plugin.connect()
+  console.log('plugin connected')
 
-  const receiverSecret = Buffer.from('secret_seed')
-  const { sharedSecret, destinationAccount } = ILP.PSK.generateParams({
-    destinationAccount: receiver.getAccount(),
-    receiverSecret
+  const payment = await SPSP.quote(plugin, {
+    receiver: 'bob@usdledger.online',
+    sourceAmount: '1',
   })
 
-  // Note the user of this module must implement the method for
-  // communicating sharedSecret and destinationAccount from the recipient
-  // to the sender
+  console.log('got SPSP payment details:', payment)
 
-  const stopListening = await ILP.PSK.listen(receiver, { receiverSecret }, (params) => {
-    console.log('got transfer:', params.transfer)
+  // we can attach an arbitrary JSON object to the payment if we want it
+  // to be sent to the receiver.
+  payment.memo = { message: 'hello!' }
 
-    console.log('fulfilling.')
-    return params.fulfill()
-  })
-
-  // the sender can generate these, via the sharedSecret and destinationAccount
-  // given to them by the receiver.
-  const { packet, condition } = ILP.PSK.createPacketAndCondition({
-    sharedSecret,
-    destinationAccount,
-    destinationAmount: '10', // denominated in the ledger's base unit
-  })
-
-  const quote = await ILP.ILQP.quoteByPacket(sender, packet)
-  console.log('got quote:', quote)
-
-  await sender.sendTransfer({
-    id: uuid(),
-    to: quote.connectorAccount,
-    amount: quote.sourceAmount,
-    expiresAt: quote.expiresAt,
-    executionCondition: condition,
-    ilp: packet
-  })
-
-  sender.on('outgoing_fulfill', (transfer, fulfillment) => {
-    console.log(transfer.id, 'was fulfilled with', fulfillment)
-    stopListening()
-  })
+  await SPSP.sendPayment(plugin, payment)
+  console.log('receiver claimed funds!')
 })()
